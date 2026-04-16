d6365daa44f775781c8139f16419df7fc7fba43a
SricharanAsr <srmasc2006@gmail.com>
2026-04-16 12:10:27 +0530

    Refactor: Move math utils to common folder

--- utils/math_utils.py
+++ utils/math_utils.py
@@ -4,3 +4,5 @@ def calculate_jitter(base_interval_sec: float, jitter_percentage: float = 0.1) -
     """Calculates a randomized jitter applied to a base interval."""
     variation = base_interval_sec * jitter_percentage
     return base_interval_sec + random.uniform(-variation, variation)
+
+# Math utils reorganization
