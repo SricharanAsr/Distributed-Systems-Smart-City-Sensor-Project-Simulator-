3d4e47d7488d9cc85378e3f5dd8e6708d4381e28
SricharanAsr <srmasc2006@gmail.com>
2026-04-22 15:07:57 +0530

    Feat: Extend data_exporter utility enabling serialized JSON bulk payloads

--- utils/data_exporter.py
+++ utils/data_exporter.py
@@ -4,3 +4,9 @@ def export_to_csv(data, filepath):
     with open(filepath, 'w') as f:
         writer = csv.writer(f)
         writer.writerow(data)
+
+import json
+
+def export_to_json(data, filepath):
+    with open(filepath, 'w') as f:
+        json.dump(data, f)
