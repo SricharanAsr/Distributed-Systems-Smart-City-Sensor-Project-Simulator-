a7a0f99d287b8df48c0c7426bb95a6c6c533a7a2
SricharanAsr <srmasc2006@gmail.com>
2026-04-22 15:07:55 +0530

    Feat: Add argparse capabilities to sensor verification tool for CLI automation

--- tools/verify_sensors.py
+++ tools/verify_sensors.py
@@ -1,5 +1,10 @@
 import json
 import os
+import argparse
 
 def verify_config(path='config.json'):
     pass
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.parse_args()
