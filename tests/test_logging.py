d9a5eacf2eb3cc0e6220b2fcfecf18a230539d8f
SricharanAsr <srmasc2006@gmail.com>
2026-04-22 15:08:01 +0530

    Test: Expand logging suite targeting generic string formats enforcing dual-capability

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -4,3 +4,7 @@ from utils.logging_utils import setup_logging
 def test_custom_json_logger():
     logger = setup_logging('test', json_format=True)
     assert logger.name == 'test'
+
+def test_standard_logger():
+    logger = setup_logging('test2', json_format=False)
+    assert logger.name == 'test2'
